5f405e521f3295b7a7972b9a1137ad32af0893d1
Justin <justin.tseng@kingwaytek.com>
2023-12-20 17:37:29 +0800

    sta - 100. Same Tree

--- ios-challenges-justin/ViewController.swift
+++ ios-challenges-justin/ViewController.swift
@@ -33,11 +33,7 @@ public class TreeNode {
 
 class Solution {
     func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
-        if p == nil && q == nil {
-            return true
-        }
-
-        return p?.val == q?.val && isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+        return false
     }
 }
 
