7b7c976868ef1eef178e830d4f07e15a17c95e53
justin.tseng <justin.tseng@kingwaytek.com>
2024-01-25 11:15:58 +0800

    end - dependency injection

--- ios-challenges-justin/ViewController.swift
+++ ios-challenges-justin/ViewController.swift
@@ -11,54 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Example usage
-        let emailNotifier = EmailNotifier()
-        let smsNotifier = SMSNotifier()
-
-        let emailNotificationService = NotificationService(notifier: emailNotifier)
-        let smsNotificationService = NotificationService(notifier: smsNotifier)
-
-        emailNotificationService.notifyUser()
-        smsNotificationService.notifyUser()
+        // Do any additional setup after loading the view.
     }
 
 
 }
 
-// Abstraction: Notifier protocol
-protocol Notifier {
-    func sendNotification()
-}
-
-// Details: EmailNotifier, a concrete implementation of Notifier
-class EmailNotifier: Notifier {
-    func sendNotification() {
-        print("Sending notification via email")
-        // Actual email sending implementation
-    }
-}
-
-// Details: SMSNotifier, another concrete implementation of Notifier
-class SMSNotifier: Notifier {
-    func sendNotification() {
-        print("Sending notification via SMS")
-        // Actual SMS sending implementation
-    }
-}
-
-// High-level module: NotificationService, depending on Notifier abstraction
-class NotificationService {
-    private let notifier: Notifier
+protocol Dependency {
     
-    init(notifier: Notifier) {
-        self.notifier = notifier
+}
+
+// Example of Constructor Injection in Swift
+class MyClass {
+    private let dependency: Dependency
+
+    init(dependency: Dependency) {
+        self.dependency = dependency
     }
-    
-    func notifyUser() {
-        // High-level logic for notifying user
-        // ...
-        
-        // Use the injected notifier to send the notification
-        notifier.sendNotification()
-    }
+
+    // Class logic using the injected dependency
 }
