be3f74a91874ce256054b377a26f097e93eb5631
justin.tseng <justin.tseng@kingwaytek.com>
2024-01-25 10:44:33 +0800

    end - dependency inversion

--- ios-challenges-justin/ViewController.swift
+++ ios-challenges-justin/ViewController.swift
@@ -11,9 +11,54 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Example usage
+        let emailNotifier = EmailNotifier()
+        let smsNotifier = SMSNotifier()
+
+        let emailNotificationService = NotificationService(notifier: emailNotifier)
+        let smsNotificationService = NotificationService(notifier: smsNotifier)
+
+        emailNotificationService.notifyUser()
+        smsNotificationService.notifyUser()
     }
 
 
 }
 
+// Abstraction: Notifier protocol
+protocol Notifier {
+    func sendNotification()
+}
+
+// Details: EmailNotifier, a concrete implementation of Notifier
+class EmailNotifier: Notifier {
+    func sendNotification() {
+        print("Sending notification via email")
+        // Actual email sending implementation
+    }
+}
+
+// Details: SMSNotifier, another concrete implementation of Notifier
+class SMSNotifier: Notifier {
+    func sendNotification() {
+        print("Sending notification via SMS")
+        // Actual SMS sending implementation
+    }
+}
+
+// High-level module: NotificationService, depending on Notifier abstraction
+class NotificationService {
+    private let notifier: Notifier
+    
+    init(notifier: Notifier) {
+        self.notifier = notifier
+    }
+    
+    func notifyUser() {
+        // High-level logic for notifying user
+        // ...
+        
+        // Use the injected notifier to send the notification
+        notifier.sendNotification()
+    }
+}
